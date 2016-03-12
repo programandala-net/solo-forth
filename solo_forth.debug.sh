3fe6adc257d1690072fe78145e6b56b00b00d47e
programandala.net <github@programandala.net>
2016-03-12 23:54:18 +0000

    Created from development backup 201509140241
    FossilOrigin-Name: 499ef50661f52653687344f2f837af15599f0fc071d330ea1206a817b90e8378

--- solo_forth.debug.sh
+++ solo_forth.debug.sh
@@ -4,27 +4,13 @@
 
 # Launch fuse with the following breakpoints:
 
-# 0x6369 next_2_end
-# 0x6A6C do_colon_end
+# 0x6355 next
+# 0x635B next2
 
-#fuse-gtk --debugger-command "br 0x6369
-#  br 0x6A6C"  solo_forth_disk_1.mgt  &
+fuse-gtk \
+   --debugger-command "break 0x635B" \
+  solo_forth_disk_1.mgt  &
 
-#  fuse-gtk --debugger-command "br 0x7474"  solo_forth_disk_1.mgt  &
-
-# 0x5E00 cold_entry
-#    --debugger-command "br 0x5E00"  \
-#   --debugger-command "br 0x79B4" \
-
-  fuse-gtk \
-   --debugger-command "break 0x8266
-  break 0x8270
-  break 0x8275" \
-    solo_forth_disk_1.mgt  &
-
-    # --debugger-command "commands 1
-  # br 0x6672
-  # br 0x6681
-  # br 0x66A8
-  # br 0x669D
-  # end" \
+    # --debugger-command "break 0x8266
+  # break 0x8270
+  # break 0x8275" \
