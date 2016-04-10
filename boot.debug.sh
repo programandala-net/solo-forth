1926519ff487f421a871629318fbcec6cef01786
programandala.net <github@programandala.net>
2016-04-10 22:13:35 +0000

    Fix and improve the tape support
    The message "Start tape, then press any key" does not appear anymore:
    writing starts inmmediately.
    
    FossilOrigin-Name: 88242d631cbb0d4e90c1e6f810d10203e1a5ff85b834c5a851aeaf6626b041fb

--- boot.debug.sh
+++ boot.debug.sh
@@ -3,15 +3,19 @@
 # boot.debug.sh
 
 # 2015-08-16
+# 2016-04-10 updated to debug the tape support
 
 # Launch Fuse with the following breakpoints:
 
-# 0x6355 next
-# 0x635B next2
 
 fuse-gtk \
   solo_forth_disk_1.mgt \
-  --debugger-command "break 0x77EA" \
+  --debugger-command "break 0x04C2
+  break 0x053C
+  break 0x0991
+  break 0x0970
+  break 0x0984
+  break 0x833B" \
  &
 
 # break 0x798E" &
