5dea58dc2ee066c606ab83fed416940c2357b3e1
programandala.net <github@programandala.net>
2016-05-13 20:39:22 +0000

    Add `load-section`
    FossilOrigin-Name: 0bd8fcf9c068ba4a2e7cdc6be0822ec38526be1689c8a44b51d4a4b8dbdae653

---
+++ make/fb2mgt.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# fb2mgt.sh
+
+# This file is part of Solo Forth
+# http://programandala.net/en.program.solo_forth.html
+
+# --------------------------------------------------------------
+# Description
+
+# This program converts a Forth source file blocks format to an
+# MGT disk image.
+#
+# This is an alternative to fsb2mgt.sh (part of fsb, see
+# http://programandala.net/en.program.fsb2.html), which checks
+# the blocks of the FSB before creating the MGT.
+#
+# fb2mgt.sh does not check the blocks of the source file.  It is
+# intended to be used in combination with <fs2fsb.sh>.  some of
+# which may have no header lines.
+#
+# Usage
+#
+#   fb2mgt.sh filename.fb
+
+# --------------------------------------------------------------
+# Author
+
+# Marcos Cruz (programandala.net), 2016.
+
+# --------------------------------------------------------------
+# License
+
+# You may do whatever you want with this work, so long as you
+# retain every copyright, credit and authorship notice, and
+# this license.  There is no warranty.
+
+# --------------------------------------------------------------
+# History
+
+# 2016-05-13: Extracted from fsb2-mgt.sh, a converter included
+# in fsb2 (http://programandala.net/en.program.fsb2.html).
+
+# --------------------------------------------------------------
+# Error checking
+
+if [ "$#" -ne 1 ] ; then
+  echo "Convert a Forth source file from .fb to .mgt"
+  echo 'Usage:'
+  echo "  ${0##*/} sourcefile.fb"
+  exit 1
+fi
+
+if [ ! -e "$1"  ] ; then
+  echo "Error: <$1> does not exist"
+  exit 1
+fi
+
+if [ ! -f "$1"  ] ; then
+  echo "Error: <$1> is not a regular file"
+  exit 1
+fi
+
+if [ ! -r "$1"  ] ; then
+  echo "Error: <$1> can not be read"
+  exit 1
+fi
+
+if [ ! -s "$1"  ] ; then
+  echo "Error: <$1> is empty"
+  exit 1
+fi
+
+# --------------------------------------------------------------
+# Main
+
+# Get the filenames:
+
+basefilename=${1%.*}
+blocksfile=$basefilename.fb
+mgtfile=$basefilename.mgt
+
+# Get the size of the file:
+du_size=$(du -sk $blocksfile)
+
+# Extract the size from the left of the string:
+file_size=${du_size%%[^0-9]*}
+
+echo "File size=($file_size)"
+#echo "$blocksfile is $file_size Kib"
+
+if [ $file_size -gt "800" ]
+then
+  echo "Error:"
+  echo "The size of $blocksfile is $file_size KiB."
+  echo "The maximum capacity of an MGT disk image is 800 KiB."
+  exit 64
+fi
+
+# Do it:
+
+dd if=$blocksfile of=$mgtfile bs=819200 cbs=819200 conv=block,sync 2>> /dev/null
+
+# Remove the temporary file:
+
+rm -f $blocksfile
+
+# vim:tw=64:ts=2:sts=2:et:
---
+++ make/fs2fb-section.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+# fs2fb-section.sh 
+
+# This file is part of Solo Forth
+# http://programandala.net/en.program.solo_forth.html
+
+# --------------------------------------------------------------
+# Description
+
+# This program converts a Forth source file from text to blocks,
+# compacting it by removing empty lines and FSB metacomments.  This
+# makes it possible to write a Forth program in ordinary text format,
+# without the constraints imposed by the intermediate format FSB, and
+# then integrate it into the Solo Forth library disk, in order to load
+# it as a whole with `load-section name`.
+#
+# The only requisite of the source is that its first actual line of
+# code (the first line that is not empty or an FSB metacomment) must
+# be a block header, with a name as identifier (a word surrounded by
+# spaces), which will be used with `load-section`.
+
+# --------------------------------------------------------------
+# Author
+
+# Marcos Cruz (programandala.net), 2016.
+
+# --------------------------------------------------------------
+# License
+
+# You may do whatever you want with this work, so long as you
+# retain every copyright, credit and authorship notice, and
+# this license.  There is no warranty.
+
+# --------------------------------------------------------------
+# History
+
+# 2016-05-13: Start, based on <fs2fb.sh>, by the same author.
+
+# --------------------------------------------------------------
+
+if [ "$#" = "0" ]; then
+	echo 'fs2fb-section'
+	echo 'Usage:'
+	echo "  ${0##*/} <filename> [<filename>] ..."
+	exit 1
+fi
+
+for file in $*
+do
+
+  base_filename=${file%.*}
+  output_file="$base_filename.fb"
+  tmp_file=$(mktemp --suffix=.tmp $base_filename-XXX)
+
+  # Remove empty lines and metacomments:
+  grep "^.\+$" $file | \
+  grep --invert-match "^\s\+\\\\\s" | \
+  grep --invert-match "^\s\+\\\\$" \
+  > $tmp_file
+
+  # Create the blocks file:
+  dd if=$tmp_file of=$output_file bs=1024 cbs=64 conv=block,sync
+
+  rm -f $tmp_file
+
+done
+
+exit 0
