df9ba9af9ee317afe74de1283cd385ce5d0053c1
programandala.net <github@programandala.net>
2017-02-28 16:35:47 +0000

    Improve `@p` and `!p`
    FossilOrigin-Name: e81fa6b6cc7758ec3b974b65d6a70c809a029a107876197786b07eea2a29d72b

--- src/lib/memory.ports.fs
+++ src/lib/memory.ports.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201702221550
+  \ Last modified: 201702281734
 
   \ -----------------------------------------------------------
   \ Description
@@ -13,7 +13,7 @@
   \ -----------------------------------------------------------
   \ Author
 
-  \ Marcos Cruz (programandala.net), 2015, 2016.
+  \ Marcos Cruz (programandala.net), 2015, 2016, 2017.
 
   \ -----------------------------------------------------------
   \ License
@@ -29,22 +29,44 @@
   \
   \ 2016-12-20: Rename `jppushhl` to `jppushhl,` and `jpnext`
   \ to `jpnext,`, after the change in the kernel.
+  \ 
+  \ 2017-02-28: Improve `@p` and `!p`: faster and smaller.
+  \ Improve documentation.
 
 ( @p !p )
 
 [unneeded] @p ?(
 
 code @p ( a -- b )
-  E1 c,  C5 c,  48 05 + c,  40 04 + c,  ED c, 68 c,
-    \ pop hl
-    \ push bc
-    \ ld c,l
-    \ ld b,h
-    \ in l,(c)
-  C1 c,  26 c, 00 c,  jppushhl, end-code
-    \ pop bc
-    \ ld h,0x00
-    \ jp pushhl
+  D9 c, C1 c, ED c, 68 c, 26 c, 00 c, E5 c, D9 c,
+    \           ; T  B
+    \           ; -- --
+    \ exx       ; 04 01
+    \ pop bc    ; 10 01
+    \ in l,(c)  ; 12 02
+    \ ld h,0x00 ; 07 02
+    \ push hl   ; 11 01
+    \ exx       ; 04 01
+  jpnext, end-code ?)
+    \ _jp_next  ; 08 02 ; jp (ix)
+    \           ; -- --
+    \           ; 56 10 Total
+
+  \ XXX OLD -- First version:
+
+    \           ; T  B
+    \           ; -- --
+    \ pop hl    ; 10 01
+    \ push bc   ; 11 01
+    \ ld c,l    ; 04 01
+    \ ld b,h    ; 04 01
+    \ in l,(c)  ; 12 02
+    \ pop bc    ; 10 01
+    \ ld h,0x00 ; 07 02
+    \ jp pushhl ; 10 03
+    \           ; 11 01 push hl
+    \           ; -- --
+    \           ; 79 13 Total
 
   \ doc{
   \
@@ -52,23 +74,39 @@ code @p ( a -- b )
   \
   \ Input byte _b_ from port _a_.
   \
+  \ See also: `!p`, `@`, `c@`.
+  \
   \ }doc
-
-?)
 
 [unneeded] !p ?(
 
-code !p ( b a -- )
-  E1 c,  D1 c,  C5 c,  48 05 + c,  40 04 + c,  ED c, 59 c,
-    \ pop hl
-    \ pop de ; char in e
-    \ push bc
-    \ ld c,l
-    \ ld b,h
-    \ out (c),e
-  C1 c,  jpnext, end-code
-    \ pop bc
-    \ jp next
+code !p ( b a -- ) D9 c, C1 c, E1 c, ED c, 69 c, D9 c,
+    \           ; T  B
+    \           ; -- --
+    \ exx       ; 04 01
+    \ pop bc    ; 10 01
+    \ pop hl    ; 10 01
+    \ out (c),l ; 12 02
+    \ exx       ; 04 01
+  jpnext, end-code ?)
+    \ _jp_next  ; 08 02 ; jp (ix)
+    \           ; -- --
+    \           ; 48 08 Total
+
+  \ XXX OLD -- First version:
+
+    \           ; T  B
+    \           ; -- --
+    \ pop hl    ; 10 01
+    \ pop de    ; 10 01
+    \ push bc   ; 11 01
+    \ ld c,l    ; 04 01
+    \ ld b,h    ; 04 01
+    \ out (c),e ; 12 02
+    \ pop bc    ; 10 01
+    \ jp next   ; 10 03
+    \           ; -- --
+    \           ; 72 11
 
   \ doc{
   \
@@ -76,8 +114,8 @@ code !p ( b a -- )
   \
   \ Output byte _b_ to port _a_.
   \
+  \ See also: `@p`, `!`, `c!`.
+  \
   \ }doc
-
-?)
 
   \ vim: filetype=soloforth
