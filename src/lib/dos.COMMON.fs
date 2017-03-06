051ea86ce5cc5c89772c8892a42ff9be9c631ec0
programandala.net <github@programandala.net>
2017-03-06 21:11:22 +0000

    Improve the `set-block-drives` tool
    Rename `block-drives` to `#block-drives`; rename `(block-drives` to
    `block-drives`.  Reorganize the code to make `get-block-drives`
    independent from `set-block-drives`. Remove `>block-drive`. Improve
    documentation. Remove the default configuration of `block-drives` that
    was set when `set-block-drives` was compiled...
    
    FossilOrigin-Name: 8865483c993522c7d7fa43fa176aad5dd0a6bc15952e1dddc4f9f76f0ac4a0e0

--- src/lib/dos.COMMON.fs
+++ src/lib/dos.COMMON.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201703050017
+  \ Last modified: 201703062115
 
   \ -----------------------------------------------------------
   \ Description
@@ -38,185 +38,65 @@
   \
   \ 2017-03-04: Document `set-block-drives` and all related
   \ words.
+  \
+  \ 2017-03-06: Rename `block-drives` to `#block-drives`;
+  \ rename `(block-drives` to `block-drives`.  Reorganize the
+  \ code to make `get-block-drives` independent from
+  \ `set-block-drives`. Remove `>block-drive`. Improve
+  \ documentation. Remove the default configuration of
+  \ `block-drives` that was set when `set-block-drives` was
+  \ compiled.
 
-( drive )
+( drive ?drive# ?block-drive block-drives )
 
-: drive ( c1 -- c2 ) first-drive + ;
+[unneeded] drive ?\ : drive ( c1 -- c2 ) first-drive + ;
 
   \ doc{
   \
   \ drive ( c1 -- c2 )
   \
-  \ Convert drive number _c1_ (0 index) to the range required
-  \ for the current DOS.
+  \ Convert drive number _c1_ (0 index) to actual drive
+  \ identifier _c2_ (DOS dependent).
   \
-  \ See also: `first-drive`.
+  \ This word is used in order to make the code portable,
+  \ abstracting the DOS drive identifiers.
+  \
+  \ Usage example:
+
+  \ ----
+  \ \ Set the second disk drive as default:
+  \
+  \ 2 set-drive       \ on G+DOS only
+  \ 1 set-drive       \ on TR-DOS only
+  \ 'B' set-drive     \ on +3DOS only
+  \
+  \ 1 drive set-drive \ on any DOS -- portable code
+  \ ----
+
+  \ See also: `first-drive`, `max-drives`.
   \
   \ }doc
 
-( set-block-drives )
-
-create (block-drives ( -- ca ) max-drives allot
-
-  \ doc{
-  \
-  \ (block-drives  ( -- ca )
-  \
-  \ _ca_ is the address of a character table that holds the
-  \ disk drives used as block drives. This table is configured
-  \ by `set-block-drives`.  The length of the table is
-  \ `max-drives`.  The first element of the table (offset 0) is
-  \ the disk drive used for blocks from number 0 to number
-  \ `blk/disk` minus one; the second element of the table
-  \ (offset 1) the disk drive used for blocks from number
-  \ `blk/disk` to number ``blk/disk 2 * 1-``; and so on.  The
-  \ block ranges not associated to disk drives are marked with
-  \ `not-block-drive`. The number of block drives is hold in
-  \ `block-drives`.
-  \
-  \ }doc
-
-  \ XXX TODO -- Rename.
-
-$FF cconstant not-block-drive
-
-  \ doc{
-  \
-  \ not-block-drive ( -- c )
-  \
-  \ _c_ is a constant identifier used by `set-block-drives`,
-  \ `-block-drives` and other related words to mark unused
-  \ elements of `(block-drives`.
-  \
-  \ }doc
-
-: -block-drives ( -- )
-  (block-drives max-drives not-block-drive fill ;
-
-  \ doc{
-  \
-  \ -block-drives  ( -- )
-  \
-  \ Fill `(block-drives` with `not-block-drive`, making no disk
-  \ drive be used as block drive.
-  \
-  \ See also: `set-block-drives`.
-  \
-  \ }doc
-
-: >block-drive ( n -- ca ) (block-drives + ;
-
-  \ XXX TODO -- Remove.
-
-  \ doc{
-  \
-  \ >block-drive ( n -- ca )
-  \
-  \ Convert _n_ to its address in `(block-drives`.
-  \
-  \ See also: `block-drive@`, `block-drive!`.
-  \
-  \ }doc
-
-: block-drive@ ( n -- c ) >block-drive c@ ;
-
-  \ XXX TODO -- Remove.
-
-  \ doc{
-  \
-  \ block-drive@ ( n -- c ) >block-drive c@ ;
-  \
-  \ Get drive _c_ currently used as block drive number _n_.
-  \
-  \ See also: `block-drive!`, `set-block-drives`,
-  \ `>block-drive`.
-  \
-  \ }doc
-
-: block-drive! ( c n -- ) >block-drive c! ;
-
-  \ XXX TODO -- Remove.
-
-  \ doc{
-  \
-  \ block-drive! ( c n -- ) >block-drive c@ ;
-  \
-  \ Set drive _c_ as block drive number _n_.
-  \
-  \ See also: `block-drive@`, `set-block-drives`,
-  \ `>block-drive`.
-  \
-  \ }doc
-
-: ?drives ( u -- ) max-drives > #-287 ?throw ;
-
-  \ doc{
-  \
-  \ ?drives ( u -- )
-  \
-  \ If _u_ is greater than the maximum number of disk drives,
-  \ throw exception #-287 ("wrong number of drives").
-  \
-  \ See also: `set-block-drives`.
-  \
-  \ }doc
-
-: set-block-drives ( c[u]..c[1] u -- )
-  dup ?drives -block-drives
-  dup block-drives c!  max-blocks 1- last-locatable !
-      0 ?do i block-drive! loop ;
-
-  \ doc{
-  \
-  \ set-block-drives ( c[n]..c[1] n -- )
-  \
-  \ Set the block drives to the drives specified by drive
-  \ identifiers _c[n]..c[1]_. Subsequently drive _c[1]_ will be
-  \ searched first for blocks, from block 0 to `blk/disk` minus
-  \ one, and so on.
-  \
-  \ If _n_ is zero, no drive is used for blocks.
-  \
-  \ When this word is loaded, the default configuration is set,
-  \ i.e. use all drives for blocks:
-
-  \ |===
-  \ | DOS    | Default configuration
-  \
-  \ | G+DOS  | ``2 1      2 set-block-drives``
-  \ | TR-DOS | ``3 2 1 0  4 set-block-drives``
-  \ | +3DOS  | ``'B' 'A'  2 set-block-drives``
-  \ |===
-
-  \ NOTE: ``set-block-drives`` sets `last-locatable` to the
-  \ last block available on the new configuration, but
-  \ `first-locatable` is not modified.
-  \
-  \ See also: `-block-drives`, `block-drives`, `block-drive!`.
-  \
-  \ }doc
-
-[undefined] g+dos  ?\ 2 1      2 set-block-drives  -->
-[undefined] tr-dos ?\ 3 2 1 0  4 set-block-drives  -->
-[undefined] +3dos  ?\ 'B' 'A'  2 set-block-drives  -->
-
-( set-block-drives )
+[unneeded] ?drive# ?(
 
 : ?drive# ( u -- )
-  [ max-drives 1- ] 1literal > #-35 ?throw ;
+  [ max-drives 1- ] 1literal u> #-35 ?throw ; ?)
 
   \ doc{
   \
   \ ?drive# ( u -- )
   \
   \ If _u_ is greater than the maximum number of disk drives,
-  \ throw exception #-287 ("invaled block number").
+  \ throw exception #-35 ("invalid block number").
   \
-  \ See also: `set-block-drives`.
+  \ See also: `(>drive-block`, `block-drives`, `?block-drive`,
+  \ `?drives`.
   \
   \ }doc
 
-: ?block-drive ( u -- ) not-block-drive = #-35 ?throw ;
+[unneeded] ?block-drive
+
+?\ : ?block-drive ( u -- ) not-block-drive = #-35 ?throw ;
 
   \ doc{
   \
@@ -225,9 +105,48 @@ $FF cconstant not-block-drive
   \ If _u_ is `not-block-drive`, throw exception #-35 ("invalid
   \ block number").
   \
-  \ See also: `set-block-drives`.
+  \ See also: `(>drive-block`, `block-drives`, `?drive#`,
+  \ `?drives`.
   \
   \ }doc
+
+[unneeded] block-drives ?( need not-block-drive
+
+create block-drives ( -- ca ) max-drives allot
+  block-drives max-drives not-block-drive fill
+  first-drive block-drives c!
+
+  \ doc{
+  \
+  \ block-drives  ( -- ca )
+  \
+  \ _ca_ is the address of a character table that holds the
+  \ disk drives used as block drives. This table can be
+  \ configured manually or using `set-block-drives`.
+  \
+  \ The length of the table is `max-drives`.  The first element
+  \ of the table (offset 0) is the disk drive used for blocks
+  \ from number 0 to number ``blk/disk 1-``; the second element
+  \ of the table (offset 1) the disk drive used for blocks from
+  \ number `blk/disk` to number ``blk/disk 2 * 1-``; and so on.
+  \
+  \ The number of used block drives is hold in `#block-drives`.
+  \
+  \ The block ranges not associated to disk drives are marked
+  \ with $FF (the `not-block-drive` optional constant is
+  \ provided for convenience), and all of them should be at the
+  \ end of the table.  In theory it's possible to define gaps
+  \ in the whole range of blocks associated to disk drives, but
+  \ this would cause trouble with `set-block-drives` and
+  \ `get-block-drives`, which use `#block-drives` as the drives
+  \ count from the start of `block-drives`.
+  \
+  \ The default configuration of ``block-drives`` is: use only
+  \ the first disk drive for blocks.
+  \
+  \ }doc
+
+need ?drive# need block-drive@ need ?block-drive
 
 : (>drive-block ( u1 -- u2 )
   blk/disk /mod ( block drive ) dup ?drive#
@@ -237,32 +156,135 @@ $FF cconstant not-block-drive
   \
   \ (>drive-block ( u1 -- u2 )
   \
-  \ Convert block _u1_ to its equivalent _u2_ in its corresponding
-  \ disk drive, which is set the current drive.
+  \ Convert block _u1_ to its equivalent _u2_ in its
+  \ corresponding disk drive, which is set the current drive.
   \
-  \ ``(>drive-block`` is the action of `>drive-block` after
-  \ `set-block-drives` has been loaded.
+  \ ``(>drive-block`` becomes the action of `>drive-block`
+  \ after `block-drives` has been loaded.
+  \
+  \ See also: `?drive#`, `?block-drive`, `set-drive`,
+  \ `set-block-drives`.
   \
   \ }doc
 
-' (>drive-block ' >drive-block defer!
+' (>drive-block ' >drive-block defer! ?)
 
-( get-block-drives )
+( not-block-drive -block-drives block-drive@ block-drive! )
 
-need set-block-drives
+[unneeded] not-block-drive ?\ $FF cconstant not-block-drive
 
-  \ XXX FIXME -- At the moment, `get-block-drives` depends on
-  \ `set-block-drives`. The problem is `(block-drives`,
-  \ contrary to `block-drives`, is not in the kernel. So, if
-  \ `get-block-drives` was loaded before `set-block-drives`,
-  \ and `(block-drives` could be required without
-  \ `set-block-drives`, `block-drives` would contain 1 but
-  \ `(block-drives` would be empty... The simplest solution is
-  \ to move `(block-drives` to the kernel and combine it with
-  \ `block-drive` into a single area.
+  \ doc{
+  \
+  \ not-block-drive ( -- c )
+  \
+  \ _c_ is a constant identifier used by `set-block-drives`,
+  \ `-block-drives` and other related words to mark unused
+  \ elements of `block-drives`.
+  \
+  \ }doc
+
+[unneeded] -block-drives ?( need block-drives
+                            need not-block-drive
+
+: -block-drives ( -- )
+  block-drives max-drives not-block-drive fill ; ?)
+
+  \ doc{
+  \
+  \ -block-drives  ( -- )
+  \
+  \ Fill `block-drives` with `not-block-drive`, making no disk
+  \ drive be used as block drive.
+  \
+  \ See also: `set-block-drives`, `get-block-drives`.
+  \
+  \ }doc
+
+[unneeded] block-drive@ ?( need block-drives
+
+: block-drive@ ( n -- c ) block-drives + c@ ; ?)
+
+  \ doc{
+  \
+  \ block-drive@ ( n -- c ) >block-drive c@ ;
+  \
+  \ Get drive _c_ (DOS dependent) currently used as block drive
+  \ number _n_ (0 index).
+  \
+  \ See also: `block-drive!`, `get-block-drives`.
+  \
+  \ }doc
+
+[unneeded] block-drive@ ?( need block-drives
+
+: block-drive! ( c n -- ) block-drives + c! ; ?)
+
+  \ doc{
+  \
+  \ block-drive! ( c n -- ) >block-drive c@ ;
+  \
+  \ Set drive _c_ (DOS dependent) as block drive number _n_ (0
+  \ index).
+  \
+  \ See also: `block-drive@`, `set-block-drives`.
+  \
+  \ }doc
+
+( ?drives set-block-drives get-block-drives )
+
+[unneeded] ?drives
+
+?\ : ?drives ( u -- ) max-drives > #-287 ?throw ;
+
+  \ doc{
+  \
+  \ ?drives ( u -- )
+  \
+  \ If _u_ is greater than the maximum number of disk drives,
+  \ throw exception #-287 ("wrong number of drives").
+  \
+  \ See also: `set-block-drives`.  `?block-drive`, `?drive#`.
+  \
+  \ }doc
+
+[unneeded] set-block-drives ?(
+
+need ?drives need -block-drives need block-drive!
+
+: set-block-drives ( c[u]..c[1] u -- )
+  dup ?drives -block-drives
+  dup #block-drives c!  max-blocks 1- last-locatable !
+      0 ?do i block-drive! loop ; ?)
+
+  \ doc{
+  \
+  \ set-block-drives ( c[n]..c[1] n -- )
+  \
+  \ Set the block drives to the drives specified by drive
+  \ identifiers _c[n]..c[1]_. Subsequently drive _c[1]_ will be
+  \ searched first for blocks from block number 0 to block
+  \ number ``blk/disk 1-``; drive _c[n+1]_ will be searched far
+  \ blocks from block number `blk/disk` to block number
+  \ ``blk/disk 2 * 1-``; and so on.
+  \
+  \ If _n_ is zero, no drive is used for blocks.
+  \
+  \ NOTE: ``set-block-drives`` sets `last-locatable` to the
+  \ last block available on the new configuration, but
+  \ `first-locatable` is not modified.
+  \
+  \ See also: `-block-drives`, `#block-drives`, `block-drive!`,
+  \ `get-block-drives`.
+  \
+  \ }doc
+
+[unneeded] get-block-drives ?(
+
+need block-drive@
 
 : get-block-drives ( -- c[u]..c[1] u )
-  block-drives c@ dup 0 ?do dup i - 1- block-drive@ swap loop ;
+  #block-drives c@
+  dup 0 ?do dup i - 1- block-drive@ swap loop ; ?)
 
   \ doc{
   \
@@ -289,8 +311,8 @@ need set-block-drives
   \ | TR-DOS | ``3 2 1 0  4 set-block-drives``
   \ | +3DOS  | ``'B' 'A'  2 set-block-drives``
   \ |===
-  \
-  \ See also: `-block-drives`, `block-drives`, `block-drive!`.
+
+  \ See also: `-block-drives`, `#block-drives`, `block-drive!`.
   \
   \ }doc
 
