179585f066ce918b393d2cbcbb0f5522e1ae665e
programandala.net <github@programandala.net>
2017-05-05 20:44:34 +0000

    Rename/move Fuse boot scripts to the tools dir
    FossilOrigin-Name: 44eb91f9ffc3a8b25432ea8f3919ed7dc2cbe087e01e01a2b839b19ae2235376

--- tools/boot_with_fuse.sh
+++ tools/boot_with_fuse.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# boot.sh
+# boot_with_fuse.sh
 
 # This file is part of Solo Forth
 # http://programandala.net/en.program.solo_forth.html
@@ -10,6 +10,8 @@
 # 2015-06: Start
 # 2016-08-05: Update with directory.
 # 2016-10-27: Update filename.
+# 2017-04-28: Update filename.
+# 2017-05-05: Update name and path.
 
 # ==============================================================
 
@@ -19,6 +21,6 @@ fuse-sdl \
 	--graphics-filter 3x \
 	--no-divide \
 	--plusd \
-  --plusddisk ./disks/gplusdos/disk0.mgt \
+  --plusddisk ./disks/gplusdos/disk_0_boot.mgt \
 	$* \
 	&
--- tools/boot_with_fuse_for_debugging.sh
+++ tools/boot_with_fuse_for_debugging.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-# boot.debug.sh
+# boot_with_fuse_for_debugging.sh
 
 # This file is part of Solo Forth
 # http://programandala.net/en.program.solo_forth.html
@@ -10,14 +10,18 @@
 # 2015-08-16: Start.
 # 2016-08-05: Update with directories.
 # 2016-10-27: Update filename.
+# 2017-04-28: Update filename.
+# 2017-05-05: Update name and path.
 
 # ==============================================================
 
 # 2016-04-20: Debug the ROM calculator support
+
 fuse-gtk \
-  disks/gplusdos/disk0.mgt \
+  ./disks/gplusdos/disk_0_boot.mgt \
   --debugger-command "break w 0x692A"\
  &
+
 # fuse-gtk \
 #   disks/gplusdos/disk0.mgt \
 #   --debugger-command "break 0x336C
