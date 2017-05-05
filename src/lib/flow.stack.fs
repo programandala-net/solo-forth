90daf6e0620be17fcf8fa6f6ac982823f6d5676c
programandala.net <github@programandala.net>
2017-05-05 14:46:51 +0000

    Make control-flow-stack words compile-only;doc them
    FossilOrigin-Name: fd0d7d54606cd89066576d3bd9d926031032a81ea7b627f391a4d1341ea0a227

--- src/lib/flow.stack.fs
+++ src/lib/flow.stack.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201702220020
+  \ Last modified: 201705051643
   \ See change log at the end of the file
 
   \ ===========================================================
@@ -26,16 +26,99 @@
 ( cs-pick cs-roll cs-swap cs-drop )
 
 [unneeded] cs-pick
-?\ need alias need pick ' pick alias cs-pick
+?\ need alias need pick ' pick alias cs-pick compile-only
+
+  \ doc{
+  \
+  \ cs-pick
+  \   \ ( S: u -- )
+  \   \ ( C: orig[u]|dest[u]..orig[0]|dest[0] --
+  \          orig[u]|dest[u]..orig[0]|dest[0] orig[u]|dest[u] )
+
+  \
+  \ Remove _u_. Copy _orig[u]|dest[u]_ to the top of the
+  \ control-flow stack.
+  \
+  \ ``cs-pick`` is a `compile-only` word.
+  \
+  \ NOTE: In Solo Forth the control-flow stack is implemented
+  \ using the data stack. Therefore ``cs-pick`` is an `alias`
+  \ of `pick`.
+  \
+  \ Origin: Forth-94 (TOOLS EXT), Forth-2012 (TOOLS EXT).
+  \
+  \ See also: `cs-roll`, `cs-swap`, `cs-drop`.
+  \
+  \ }doc
 
 [unneeded] cs-roll
-?\ need alias need roll ' roll alias cs-roll
+?\ need alias need roll ' roll alias cs-roll compile-only
+
+  \ doc{
+  \
+  \ cs-roll
+  \   \ ( S: u -- )
+  \   \ ( C: orig[u]|dest[u] orig[u-1]|dest[u-1]..orig[0]|dest[0] --
+  \   \      orig[u-1]|dest[u-1]..orig[0]|dest[0] orig[u]|dest[u] )
+
+  \
+  \ Remove _u_.  Rotate _u+1_ items on top of the control-flow
+  \ stack so that _orig[u]|dest[u]_ is on top of the
+  \ control-flow stack.
+  \
+  \ ``cs-roll`` is a `compile-only` word.
+  \
+  \ NOTE: In Solo Forth the control-flow stack is implemented
+  \ using the data stack. Therefore ``cs-roll`` is an `alias`
+  \ of `roll`.
+  \
+  \ Origin: Forth-94 (TOOLS EXT), Forth-2012 (TOOLS EXT).
+  \
+  \ See also: `cs-pick`, `cs-swap`, `cs-drop`.
+  \
+  \ }doc
 
 [unneeded] cs-swap
-?\ need alias ' swap alias cs-swap
+?\ need alias ' swap alias cs-swap compile-only
+
+  \ doc{
+  \
+  \ cs-swap
+  \   \ ( C: orig[1]|dest[1] orig[2]|dest[2] --
+  \   \      orig[2]|dest[2] orig[1]|dest[1] )
+
+  \
+  \ Exchange the top two control-flow stack items.
+  \
+  \ ``cs-swap`` is a `compile-only` word.
+  \
+  \ NOTE: In Solo Forth the control-flow stack is implemented
+  \ using the data stack. Therefore ``cs-swap`` is an `alias`
+  \ of `swap`.
+  \
+  \ See also: `cs-pick`, `cs-roll`, `cs-drop`.
+  \
+  \ }doc
 
 [unneeded] cs-drop
-?\ need alias ' drop alias cs-drop
+?\ need alias ' drop alias cs-drop compile-only
+
+  \ doc{
+  \
+  \ cs-drop ( C: orig|dest -- )
+
+  \
+  \ Remove _orig|dest_ from the control-flow stack.
+  \
+  \ ``cs-drop`` is a `compile-only` word.
+  \
+  \ NOTE: In Solo Forth the control-flow stack is implemented
+  \ using the data stack. Therefore ``cs-drop`` is an `alias`
+  \ of `drop`.
+  \
+  \ See also: `cs-pick`, `cs-roll`, `cs-swap`.
+  \
+  \ }doc
 
   \ ===========================================================
   \ Change log
@@ -44,5 +127,8 @@
   \ `cs-drop` individually accessible to `need`.  Remove
   \ alternative unfinished implementations, ported from
   \ DX-Forth and hForth.
+  \
+  \ 2017-05-05: Make `cs-pick`, `cs-roll`, `cs-swap` and
+  \ `cs-drop` compile-only. Document them.
 
   \ vim: filetype=soloforth
