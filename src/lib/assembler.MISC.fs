a3d3920247de96f4b688d9764e52442caf624ef8
programandala.net <github@programandala.net>
2017-05-09 14:42:19 +0000

    Rename the assembler tools module
    FossilOrigin-Name: 6bb69b3c3bf3ea415ceeced798cea4c2f95d040ffe2050aa92b2ab4c56feccaf

--- src/lib/assembler.MISC.fs
+++ src/lib/assembler.MISC.fs
@@ -1,26 +1,22 @@
-  \ assembler.tools.fs
+  \ assembler.MISC.fs
   \
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ XXX UNDER DEVELOPMENT
-
-  \ Last modified: 201702220028
+  \ Last modified: 201705091336
 
   \ ===========================================================
   \ Description
 
-  \ Z80 assembler tools, independent from the actual assembler.
+  \ Z80 assembler misc words, independent from the actual
+  \ assembler.
 
   \ ===========================================================
   \ Authors
 
-  \ Original code by Frank Sergeant, for Pygmy Forth.
-  \
-  \ Adapted to Solo Forth by Marcos Cruz (programandala.net),
-  \ 2015, 2016.
+  \ Marcos Cruz (programandala.net), 2015, 2016, 2017
 
-  \ ===========================================================--
+  \ ===========================================================
   \ License
 
   \ You may do whatever you want with this work, so long as you
@@ -29,21 +25,32 @@
 
 ( << >> )
 
-  \ For dumping assembled code to screen.
+[unneeded] << [unneeded] >> and (?
+
+  \ XXX UNDER DEVELOPMENT
+
+  \ Tool to dump assembled code to screen.
 
   \ Credit:
   \
+  \ Original code by Frank Sergeant, for Pygmy Forth.
   \ Code adapted from Pygmy Forth.
-
-  \ XXX TODO finish adapt
 
 need @c+ need for
 
 : << ( -- a depth ) here depth ;
+
 : >> ( a depth -- )
   depth 1- - #-258 ?throw cr base @ >r hex
   dup 4 u.r space  here over - for  c@+ 3 u.r  step drop
-  r> base !  space ;
+  r> base !  space ; ?)
+
+  \ ===========================================================--
+  \ Change log
+
+  \ 2015, 2016: Unfinished conversion of Pygmy Forth's `>>`.
+  \
+  \ 2017-05-09: Rename the file.
 
   \ vim: filetype=soloforth
 
