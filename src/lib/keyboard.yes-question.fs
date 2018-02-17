0703d6cb2f1af77c7d33caad7e1b8ffbef8a9066
programandala.net <github@programandala.net>
2018-02-17 16:56:28 +0000

    Improve needing&documentation of `y/n?` module
    FossilOrigin-Name: ac46f1be4166d0a11b73bb50de68beb0141200dfd511fd1bbd51d56efc2e17ec

--- src/lib/keyboard.yes-question.fs
+++ src/lib/keyboard.yes-question.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201702220020
+  \ Last modified: 201802171710
   \ See change log at the end of the file
 
   \ ===========================================================
@@ -14,7 +14,7 @@
   \ ===========================================================
   \ Author
 
-  \ Marcos Cruz (programandala.net), 2015, 2016.
+  \ Marcos Cruz (programandala.net), 2015, 2016, 2017, 2018.
 
   \ ===========================================================
   \ License
@@ -23,35 +23,104 @@
   \ retain every copyright, credit and authorship notice, and
   \ this license.  There is no warranty.
 
-( y/n? y/n no? yes? )
+( "y" "n" y/n? y/n no? yes? )
 
   \ Credit:
   \
   \ Code adapted from Afera.
 
-need value
+[unneeded] "y" [unneeded] "n" and ?(
 
-'y' value "y"  'n' value "n"
-  \ Default (uppercase) letters for "yes" and "no".
+'y' cconstant "y"
 
-: y/n? ( c -- f ) dup "y" = swap "n" = or ;
-  \ Is the given (uppercase) char _c_
-  \ a valid answer for a "y/n" question?
+  \ doc{
+  \
+  \ "y" ( -- c ) "quote-y"
+  \
+  \ A character constant containing the (lowercase) character
+  \ used by `y/n`, `y/n?` and `yes?`, to represent an
+  \ affirmative answer. By default it's "y".  For localization,
+  \ the value can be changed with `c!>`.
+  \
+  \ See: `"n"`.
+  \
+  \ }doc
 
-: y/n ( -- c )
-  begin  key lower dup y/n? 0=  while  drop  repeat ;
-  \ Wait for a valid key press for a "y/n" question
-  \ and return its code.
+'n' cconstant "n" ?)
 
-: no? ( -- f ) y/n "n" = ;
-  \ Wait for a valid key press for a "y/n" question
-  \ and return _true_ if it's the current letter for "no",
+  \ doc{
+  \
+  \ "n" ( -- c ) "quote-n"
+  \
+  \ A character constant containing the (lowercase) character
+  \ used by `y/n`, `y/n?` and `no?` to represent a negative
+  \ answer. By default it's "n".  For localization, the value
+  \ can be changed with `c!>`.
+  \
+  \ See: `"y"`.
+  \
+  \ }doc
+
+[unneeded] y/n? ?( need "y" need "n"
+
+: y/n? ( c -- f ) lower dup "y" = swap "n" = or ; ?)
+
+  \ doc{
+  \
+  \ y/n? ( c -- f ) "y-slash-n-question"
+  \
+  \ Is character _c_, converted to lowercase, a valid answer
+  \ for a "y/n" question? I.e., is _c_ the current value of
+  \ `"y"` or `"n"`?
+  \
+  \ See: `yes?`, `no?`, `y/n`.
+  \
+  \ }doc
+
+[unneeded] y/n ?( need y/n?
+
+: y/n ( -- c ) begin key dup y/n? 0= while drop repeat ; ?)
+
+  \ doc{
+  \
+  \ y/n ( -- c ) "y-slash-n"
+  \
+  \ Wait for a valid `key` press for a "yes/no" question and
+  \ return its code _c_, which is `"y"` or `"n"`.
+  \
+  \ See: `y/n?`.
+  \
+  \ }doc
+
+[unneeded] no? ?\ need y/n need "n" : no? ( -- f ) y/n "n" = ;
+
+  \ doc{
+  \
+  \ no? ( -- f ) "no-question"
+  \
+  \ Wait for a valid `key` press for a `y/n` question
+  \ and return _true_ if it's the current value of `"n"`,
   \ else return _false_.
+  \
+  \ See: `yes?`, `y/n?`.
+  \
+  \ }doc
 
-: yes? ( -- f ) y/n "y" = ;
-  \ Wait for a valid key press for a "y/n" question
-  \ and return _true_ if it's the current letter for "yes",
-  \ else return _false_.
+[unneeded] yes?
+
+?\ need y/n need "y" : yes? ( -- f ) y/n "y" = ;
+
+  \ doc{
+  \
+  \ yes? ( -- f ) "yes-question"
+  \
+  \ Wait for a valid `key` press for a `y/n` question and
+  \ return _true_ if it's the current value of `"y"`, else
+  \ return _false_.
+  \
+  \ See: `no?`, `y/n?`.
+  \
+  \ }doc
 
   \ ===========================================================
   \ Change log
@@ -61,5 +130,9 @@ need value
   \
   \ 2017-02-01: Replace `upper` with `lower`, because `upper`
   \ has been moved to the library.
+  \
+  \ 2018-02-17: Replace `value` with `cconstant`. Fully
+  \ document, including pronunciation. Make all words
+  \ individually accessible to `need`.
 
   \ vim: filetype=soloforth
