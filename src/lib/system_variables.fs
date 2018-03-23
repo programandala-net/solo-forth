2f7a317b56051781856d745cea38ea758d46a8d8
programandala.net <github@programandala.net>
2018-03-23 11:23:39 +0000

    Add `os-streams`; improve doc of `os-chans`
    FossilOrigin-Name: dddc3bb5b292d122f8fab2c96b911f88ecb73e2bfd6b92837b294b490e6d6199

--- src/lib/system_variables.fs
+++ src/lib/system_variables.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 201803091350
+  \ Last modified: 201803231222
   \ See change log at the end of the file
 
   \ ===========================================================
@@ -46,8 +46,43 @@ unneeding os-chans ?\ #23631 constant os-chans
   \
   \ os-chans ( -- a ) "o-s-chans"
   \
-  \ A constant that returns the address _a_ of system variable
-  \ CHANS, which holds the address of channels data.
+  \ A constant that returns the address _a_ of the system
+  \ variable CHANS, which holds the address of the channel data
+  \ table. Each element of the table has the following
+  \ structure:
+
+  \ |===
+  \ | Offset (bytes) | Content
+  \
+  \ | +0             | Address of the channel output routine
+  \ | +2             | Address of the channel input routine
+  \ | +4             | Channel identifier character
+  \ |===
+
+  \ The default contents of the channel data table are the
+  \ following:
+
+  \ |===
+  \ | Offset (bytes) | Content
+  \
+  \ | +0             | $09F4 (print-out)
+  \ | +2             | $10A8 (key-input)
+  \ | +4             | 'K'
+  \ | +5             | $09F4 (print-out)
+  \ | +7             | $15C4 (report-j)
+  \ | +9             | 'S'
+  \ | +10            | $08F1 (add-char)
+  \ | +12            | $15C4 (report-j)
+  \ | +14            | 'R'
+  \ | +15            | $09F4 (print-out)
+  \ | +17            | $15C4 (report-j)
+  \ | +19            | 'P'
+  \ |===
+
+  \ NOTE: The elements of the channel data table are pointed
+  \ from `os-strms` by 1-indexed byte offsets, i.e. $0001
+  \ points to the first element of the channel data table,
+  \ channel 'K'.
   \
   \ }doc
 
@@ -110,7 +145,7 @@ unneeding os-udg ?\ #23675 constant os-udg
   \
   \ }doc
 
-( os-coords os-coordx os-coordy )
+( os-coords os-coordx os-coordy os-strms )
 
 unneeding os-coords ?\ #23677 constant os-coords
 
@@ -132,11 +167,41 @@ unneeding os-coordx ?\ #23677 constant os-coordx
   \
   \ os-coordx ( -- ca ) "o-s-coord-x"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable COORDX which holds the graphic x coordinate of the
   \ last point plotted.
   \
   \ See: `set-pixel`, `plot`, `os-coords`, `os-coordy`.
+  \
+  \ }doc
+
+unneeding os-strms ?\ #23568 constant os-strms
+
+  \ doc{
+  \
+  \ os-strms ( -- a ) "o-s-streams"
+  \
+  \ A constant that returns the address _a_ of a 38-byte
+  \ (19-cell) system variable STRMS which holds one cell per
+  \ stream, containing the address of the channel attached to
+  \ it, as follows:
+
+  \ |===
+  \ | Offset (cells) | Stream | Content
+  \
+  \ | +0             | -3     | $0001 (offset to channel 'K')
+  \ | +1             | -2     | $0006 (offset to channel 'S')
+  \ | +2             | -1     | $000B (offset to channel 'R')
+  \ | +3             | 0      | $0001 (offset to channel 'K')
+  \ | +4             | 1      | $0001 (offset to channel 'K')
+  \ | +5             | 2      | $0006 (offset to channel 'S')
+  \ | +6             | 3      | $0010 (offset to channel 'P')
+  \ | +7..+18        | 4..15  | $0000..$0000 (not attached)
+  \ |===
+
+  \ NOTE: The contents are 1-index offsets from the address
+  \ `os-chans`.  When the content of a stream cell is zero, the
+  \ stream is not attached to a channel.
   \
   \ }doc
 
@@ -146,7 +211,7 @@ unneeding os-coordy ?\ #23678 constant os-coordy
   \
   \ os-coordy ( -- ca ) "o-s-coord-y"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable COORDY which holds the graphic y coordinate of the
   \ last point plotted.
   \
@@ -162,7 +227,7 @@ unneeding os-attr-p ?\ #23693 constant os-attr-p
   \
   \ os-attr-p ( -- ca ) "o-s-attribute-p"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable ATTR_P, which holds the current permanent color
   \ attribute, as set up by color statements.
   \
@@ -176,7 +241,7 @@ unneeding os-mask-p ?\ #23694 constant os-mask-p
   \
   \ os-mask-p ( -- ca ) "o-s-mask-p"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable MASK_P, which holds the permanent color attribute
   \ mask, used for transparent colors, etc. Any bit that is 1
   \ shows that the corresponding attribute bit is taken not
@@ -192,7 +257,7 @@ unneeding os-attr-t ?\ #23695 constant os-attr-t
   \
   \ os-attr-t ( -- ca ) "o-s-attribute-t"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable ATTR_T, which holds the current temporary color
   \ attribute, as set up by color statements.
   \
@@ -206,7 +271,7 @@ unneeding os-mask-t ?\ #23696 constant os-mask-t
   \
   \ os-mask-t ( -- ca ) "o-s-mask-t"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable MASK_T, which holds the temporary color attribute
   \ mask, used for transparent colors, etc. Any bit that is 1
   \ shows that the corresponding attribute bit is taken not
@@ -223,7 +288,7 @@ unneeding os-p-flag ?\ #23697 constant os-mask-t
   \
   \ os-p-flag ( -- ca ) "o-s-p-flag"
   \
-  \ A constant that returns the address _a_ of 1-byte system
+  \ A constant that returns the address _ca_ of 1-byte system
   \ variable P_FLAG, which holds some flags related to
   \ printing.
   \
@@ -253,5 +318,8 @@ unneeding os-p-flag ?\ #23697 constant os-mask-t
   \ 2018-03-05: Update `[unneeded]` to `unneeding`.
   \
   \ 2018-03-09: Add words' pronunciaton.
+  \
+  \ 2018-03-23: Add `os-strms`. Improve documentation of
+  \ `os-chans`. Fix documentation.
 
   \ vim: filetype=soloforth
