802692806563e76084a66739312375833a326756
programandala.net <github@programandala.net>
2019-03-15 13:06:46 +0000

    Fix and rewrite fs2fba.sh
    FossilOrigin-Name: 1f22e745f9474dcda6ca73044a32f3f86b6109bb35d3fb2433d73701204d48fa

--- make/fs2fba.sh
+++ make/fs2fba.sh
@@ -4,7 +4,22 @@
 # This file is part of Solo Forth
 # http://programandala.net/en.program.solo_forth.html
 
-# --------------------------------------------------------------
+# Last modified 201903151335.
+# See change log at the end of the file.
+
+# ==============================================================
+# Author
+
+# Marcos Cruz (programandala.net), 2016, 2019.
+
+# ==============================================================
+# License
+
+# You may do whatever you want with this work, so long as you
+# retain every copyright, credit and authorship notice, and
+# this license.  There is no warranty.
+
+# ==============================================================
 # Description
 
 # This program converts a Forth source file from text to blocks,
@@ -18,33 +33,29 @@
 # code (the first line that is not empty or an FSB metacomment) must
 # be a block header, with a name as identifier (a word surrounded by
 # spaces), which will be used with `load-app`.
+#
+# This program is intended to be copied to the projects written in
+# Solo Forth and called from Makefile.
+#
+# See the Makefile of projects _Black Flag_
+# (http://programandala.net/en.program.black_flag.html) and _Nuclear
+# Waste Invaders_
+# (http://programandala.net/en.program.nuclear_waste_invaders.html)
+# for an usage example.
 
-# --------------------------------------------------------------
-# Author
+# ==============================================================
+# Requisites
 
-# Marcos Cruz (programandala.net), 2016.
+# - vim
+# - dd
 
-# --------------------------------------------------------------
-# License
-
-# You may do whatever you want with this work, so long as you
-# retain every copyright, credit and authorship notice, and
-# this license.  There is no warranty.
-
-# --------------------------------------------------------------
-# History
-
-# 2016-05-13: Start, based on <fs2fb.sh>, by the same author.
-# 2016-06-01: Fix description.
-# 2016-12-19: Fix problem with grep.
-
-# --------------------------------------------------------------
+# ==============================================================
 
 if [ "$#" = "0" ]; then
-	echo 'fs2fba'
-	echo 'Usage:'
-	echo "  ${0##*/} <filename> [<filename>] ..."
-	exit 1
+  echo 'fs2fba'
+  echo 'Usage:'
+  echo "  ${0##*/} <filename> [<filename>] ..."
+  exit 1
 fi
 
 for file in $*
@@ -54,34 +65,13 @@ do
   output_file="$base_filename.fba"
   tmp_file=$(mktemp --suffix=.tmp $base_filename-XXX)
 
-  # Remove empty lines and metacomments:
-
-  # XXX OLD -- 2016-12-19: this expression removes lines that contain
-  # Latin1 non-ASCII chars, but it works fine with UTF-8 texts, why?:
-  #
-  # grep "^.\+$" $file | \
-
-  # XXX NEW -- 2016-12-19: the problem is solved when `$` is removed,
-  # why?:
-  grep "^.\+" $file | \
-  grep --invert-match "^\s\+\\\\\s" | \
-  grep --invert-match "^\s\+\\\\$" \
-  > $tmp_file
-
-  # ################################################
-  # XXX TMP -- debugging test:
-
-  # # XXX FIXME -- this removes lines that contain latin1 non-ASCII
-  # # chars, why?!:
-  # grep "^.\+$" $file > src/XXX-step-1-grep.tmp
-  # # XXX FIXME -- ack creates an empty file, why?
-  # ack "^.\+$" $file > src/XXX-step-1-ack.tmp
-  # # XXX FIXME -- no difference:
-  # grep --extended-regexp "^.+$" $file > src/XXX-step-grep-1E.tmp
-  # # XXX REMARK -- solution:
-  # grep "^.\+" $file > src/XXX-step-1-grep-1D.tmp
-
-  # ################################################
+  # Remove empty lines and meta line comments (backslash line
+  # comments preceded by at least one space):
+  vim -e \
+    -c "%s@^\\(\\s\\+\\\\.*\\)\\?\\n@@e" \
+    -c "saveas! $tmp_file" \
+    -c "quit!" \
+    $file ; \
 
   # Create the blocks file:
   dd if=$tmp_file of=$output_file bs=1024 cbs=64 conv=block,sync
@@ -91,3 +81,18 @@ do
 done
 
 exit 0
+
+# ==============================================================
+# Change log
+
+# 2016-05-13: Start, based on <fs2fb.sh>, by the same author.
+#
+# 2016-06-01: Fix description.
+#
+# 2016-12-19: Fix problem with grep.
+#
+# 2019-03-15: Fix and rewrite. grep was causing problems again: This
+# step removed empty lines, but somehow it removed also all code lines
+# containing non-ASCII characters!: `grep "^.\+" $file`.  This
+# alternative made no difference: `grep --invert-match "^$" $file`.
+# Finally grep has been replaced by Vim.  Improve documentation.
