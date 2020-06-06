661feef700407f6db7e76385744717f61243f7a9
programandala.net <github@programandala.net>
2020-06-06 15:24:05 +0000

    Add `dump-wordlists` and factors
    FossilOrigin-Name: cc6b84509a50e13787bca8b070d9e01faaebf94afe8f2736e66a76d1578ae535

--- src/lib/tool.list.word_lists.fs
+++ src/lib/tool.list.word_lists.fs
@@ -3,7 +3,7 @@
   \ This file is part of Solo Forth
   \ http://programandala.net/en.program.solo_forth.html
 
-  \ Last modified: 202005080127
+  \ Last modified: 202006061722
   \ See change log at the end of the file
 
   \ ===========================================================
@@ -24,7 +24,7 @@
   \ retain every copyright, credit and authorship notice, and
   \ this license.  There is no warranty.
 
-( .current .context .wordlist )
+( .current .context .wordlist dump-wordlist order )
 
 unneeding .current
 ?\ : .current ( -- ) get-current .wordlist ;
@@ -42,7 +42,7 @@ unneeding .current
 unneeding .context ?( need .wordlist
 
 : .context ( -- )
-  get-order begin  ?dup  while  swap .wordlist 1-  repeat ; ?)
+  get-order begin ?dup while swap .wordlist 1- repeat ; ?)
 
   \ doc{
   \
@@ -58,40 +58,36 @@ unneeding .context ?( need .wordlist
 unneeding .wordlist ?( need wordlist>name need .name
 
 : .wordlist ( wid -- )
-  dup wordlist>name @ ?dup if  .name drop exit  then  u. ; ?)
+  dup wordlist>name @ ?dup if .name drop exit then u. ; ?)
 
   \ doc{
   \
   \ .wordlist ( wid -- ) "dot-wordlist"
   \
-  \ If the word list identified by _wid_ has an associated
+  \ If the `wordlist` identified by _wid_ has an associated
   \ name, display it; else display _wid_.
   \
-  \ See: `wordlists`.
+  \ See: `wordlists`, `dump-wordlist`, `wordlist>name`.
   \
   \ }doc
 
-( wordlists order )
+unneeding dump-wordlist ?( need .wordlist need wordlist>last
+                           need .name
 
-unneeding wordlists ?( need .wordlist need wordlist>link
-
-: wordlists ( -- )
-  latest-wordlist
-  begin  @ ( wid|0) ?dup  while  dup .wordlist wordlist>link
-  repeat ; ?)
+: dump-wordlist ( wid -- ) dup cr ." Word list: " .wordlist
+  cr ." Latest definition: " wordlist>last @ .name ; ?)
 
   \ doc{
   \
-  \ wordlists ( -- )
+  \ dump-wordlist ( wid -- )
   \
-  \ List all wordlists defined in the system, either by name
-  \ (if they have an associated name) or by number (its word
-  \ list identifier, if they have no associated name). The word
-  \ lists are listed in reverse chronological order: The first
-  \ word list listed is the most recently defined.
+  \ Dump the data of the `wordlist` identified by _wid_, with
+  \ labels: its associated name (or, if none, just the _wid_)
+  \ and the name of the latest definition created in the word
+  \ list.
   \
-  \ See: `.wordlist`, `words`, `wordlist-words`, `wordlist`,
-  \ `latest-wordlist`.
+  \ See: `.wordlist`, `dump-wordlists`, `wordlist>last`,
+  \ `.name`.
   \
   \ }doc
 
@@ -118,6 +114,66 @@ unneeding order ?( need .context need .current
   \
   \ }doc
 
+( wordlists dump-wordlists> dump-wordlists )
+
+unneeding wordlists ?( need .wordlist need wordlist>link
+
+: wordlists ( -- )
+  latest-wordlist
+  begin @ ( wid|0) ?dup while dup .wordlist wordlist>link
+  repeat ; ?)
+
+  \ doc{
+  \
+  \ wordlists ( -- )
+  \
+  \ List all wordlists defined in the system, either by name
+  \ (if they have an associated name) or by number (its word
+  \ list identifier, if they have no associated name). The word
+  \ lists are listed in reverse chronological order: The first
+  \ word list listed is the most recently defined.
+  \
+  \ See: `.wordlist`, `words`, `wordlist-words`, `wordlist`,
+  \ `latest-wordlist`.
+  \
+  \ }doc
+
+unneeding dump-wordlists> ?( need dump-wordlist
+                             need wordlist>link
+
+: dump-wordlists> ( wid -- )
+  begin ( wid|0) ?dup
+  while dup dump-wordlist cr wordlist>link @ repeat ; ?)
+
+  \ doc{
+  \
+  \ dump-wordlists> ( wid -- ) "dump-wordlists-from"
+  \
+  \ Dump the data of all the word lists defined in the system,
+  \ starting from the `wordlist` identified by _wid_.
+  \
+  \ ``dump-wordlists>`` is a useful factor of `dump-wordlists`.
+  \
+  \ See: `dump-wordlist`, `wordlists`, `wordlist>link`.
+  \
+  \ }doc
+
+unneeding dump-wordlists ?( need dump-wordlists>
+
+: dump-wordlists ( -- )
+  latest-wordlist @ dump-wordlists> ; ?)
+
+  \ doc{
+  \
+  \ dump-wordlists ( -- )
+  \
+  \ Dump the data of all the word lists defined in the system,
+  \ starting from the `wordlist` pointed by `latest-wordlist`.
+  \
+  \ See: `dump-wordlist`, `dump-wordlists>`, `wordlists`.
+  \
+  \ }doc
+
   \ ===========================================================
   \ Change log
 
@@ -139,7 +195,7 @@ unneeding order ?( need .context need .current
   \ finish `wordlists`.
   \
   \ 2017-01-07: Update `wid>name` to `wordlist>name @`,
-  \ `wid>link` to `wordlist>link`,  and `wid>vocabulary` to
+  \ `wid>link` to `wordlist>link`, and `wid>vocabulary` to
   \ `wordlist>vocabulary`, after the changes in the word-list
   \ interface words. Rename `.wid` to `.wordlist`.  Rename
   \ `named-wid` to `latest>wordlist` in documentation.
@@ -160,5 +216,8 @@ unneeding order ?( need .context need .current
   \
   \ 2020-05-08: Update requirements: `.name` has been moved
   \ from the kernel to the library.
+  \
+  \ 2020-06-06: Add `dump-wordlists`, `dump-wordlists>` and
+  \ `dump-wordlist`. Update source style.
 
   \ vim: filetype=soloforth
